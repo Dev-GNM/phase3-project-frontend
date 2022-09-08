1cc8690ce0ae32a5761726b356bbad696621e06d
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 19:05:43 +0300

    handling comment submit

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -6,3 +6,25 @@ function Comments({ categoryComment, mov_id, handleAddComment }) {
     const [newComment, setNewComment] = useState("")
     console.log(newComment)
     console.log(categoryComment);
+
+    function handleSubmit(e) {
+        // debugger;
+        e.preventDefault();
+        fetch(`http://localhost:9292/${categoryComment}`, {
+            method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                comment: newComment,
+                likes: 0,
+                mov_id: mov_id
+            })
+        }).then(resp => resp.json())
+            .then(addcomment => {
+                handleAddComment(addcomment)
+                console.log(addcomment)
+                e.target.reset();
+            })
+        setNewComment(" ");
+    }
