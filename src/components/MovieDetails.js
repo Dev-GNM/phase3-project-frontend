71f09dd7df544bcd41e68662eacbe50d83b479d5
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 19:10:18 +0300

    returning movie details by category

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -18,3 +18,28 @@ function MovieDetails({ category }) {
             .then(resp => resp.json())
             .then(data => setGetMovie(data))
     }, [])
+
+
+    // const { id } = useParams()
+    // console.log(id);
+    // console.log(props.location)
+    const baseImageUrl = "https://image.tmdb.org/t/p/original/"
+    return (
+        <div style={{ backgroundColor: "black" }}>
+            <div className="shadow" style={{ marginTop: "3rem", backgroundColor: "white", position: "relative" }}>
+                <h1 className="centerText">{getMovie?.name || getMovie?.title || getMovie?.original_name}</h1>
+                <img className="center" src={`${baseImageUrl}${getMovie.poster_path}`} alt="poster" ></img>
+
+                <br />
+                <p className="centerText"> <h3>About {getMovie?.name || getMovie?.title || getMovie?.original_name}</h3>{getMovie.overview}</p>
+                <br />
+                <img className="center" src={`${baseImageUrl}${getMovie.backdrop_path}`} alt="poster" ></img>
+            </div>
+            <div>
+                <Reviews movieId={getPathId} category={category} />
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetails
