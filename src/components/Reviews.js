f8b4e5841f5b82f591ae29830b2c26a905b72011
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 19:18:03 +0300

    handling movie reviews by id

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,3 +3,11 @@ import { useEffect, useState } from 'react'
 import ReviewLikes from './ReviewLikes'
 import moment from 'moment'
 import './Review.css'
+
+function Reviews({ movieId, category }) {
+
+    const [comment, setComment] = useState([])
+    const [showComments, setShowComments] = useState(false)
+    const [likes, setLikes] = useState(0)
+
+    console.log(movieId)
