9eb8461c89618dbaaf6478992e0f6f3ff3ba570f
Geoffrey <geoffreynjihia15@gmail.com>
2022-09-08 19:09:32 +0300

    fetching by category

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,3 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Reviews from './Reviews';
 import './Review.css'
+
+function MovieDetails({ category }) {
+    console.log(category)
+
+    const getPath = window.location.pathname
+    const getPathId = parseInt(getPath.split("/").pop())
+
+
+    // debugger
+    console.log(getPath)
+    const [getMovie, setGetMovie] = useState([])
+    // console.log(window.location.pathname)
+    useEffect(() => {
+        fetch(`http://localhost:9292${getPath}`)
+            .then(resp => resp.json())
+            .then(data => setGetMovie(data))
+    }, [])
